a927c18bba233b57710cbc1231426222ed16728b
nevmx <maxsnev@gmail.com>
2016-02-28 11:36:20 -0500

    Domain objects

---
+++ HttpRequestTool/HttpRequestTool/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestTool
+{
+    class Request
+    {
+        private int id { get; set; }
+        private String requestString { get; set; }
+
+        // Constructor
+        public Request(int id, String requestString)
+        {
+            this.id = id;
+            this.requestString = requestString;
+        }
+    }
+}
